a30fd2a4c002d5b5a9a8edf9179a2b2023157fc9
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-05-11 05:40:43 -0400

    Deploy Next.js to gh-pages

---
+++ out/_next/static/r2aTZtFN25XcnK8RYRTGc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-5dda03bc010437df.js","static/chunks/pages/index-df85b325f62e67fb.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
